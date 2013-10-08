a3d2ce57d6e0c92b821bd1dec0e81d6c70a84b52
Sherwin Yu <sherwin.x.yu@gmail.com>
2013-10-08 19:16:39 -0400

    VideoCellAdapter

---
+++ src/com/sherwinyu/videotable/VideoCellAdapter.java
@@ -0,0 +1,75 @@
+package com.sherwinyu.videotable;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+
+import android.content.Context;
+
+import android.util.Log;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class VideoCellAdapter extends ArrayAdapter<VideoCell> {
+
+  Context context;
+  int layoutResourceId;
+  ArrayList<VideoCell> data = null;
+
+  public VideoCellAdapter(Context context, int layoutResourceId, ArrayList<VideoCell> data) {
+    super(context, layoutResourceId, data);
+    this.layoutResourceId = layoutResourceId;
+    this.context = context;
+    this.data = data;
+  }
+
+
+  @Override
+  public void add(VideoCell videoCell) {
+    this.data.add(videoCell);
+  }
+
+  @Override
+  public View getView(int idx, View convertView, ViewGroup parent) {
+    View rowView = convertView;
+    VideoCellHolder holder = null;
+
+    Log.v("kamcord", "VCAdapter.getView(" + idx + ")");
+
+    if (rowView == null) {
+      LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+      rowView = inflater.inflate(layoutResourceId, parent, false);
+      holder = new VideoCellHolder();
+      holder.thumbnail = (ImageView) rowView.findViewById(R.id.videoCellThumbnail);
+      holder.title = (TextView) rowView.findViewById(R.id.videoCellTitle);
+
+      Log.v("kamcord", "holder miss: " + holder.title);
+      rowView.setTag(holder);
+    }
+    else {
+      holder = (VideoCellHolder) rowView.getTag();
+      Log.v("kamcord", "holder hit: " + holder.title);
+    }
+    VideoCell videoCell = data.get(idx);
+    if (videoCell.thumbnail != null) {
+      holder.thumbnail.setImageDrawable(videoCell.thumbnail);
+      Log.v("kamcord", "getView: settingImageDrawable to " + videoCell.imageUrl);
+    } else {
+      Log.v("kamcord", "getView: noImageFound for video: " + videoCell.title);
+    }
+    holder.title.setText(videoCell.title);
+
+    return rowView;
+  }
+
+  private class VideoCellHolder {
+    ImageView thumbnail;
+    TextView title;
+  }
+}

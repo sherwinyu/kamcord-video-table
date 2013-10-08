56f46e86cb2608b1bd233db8f08be0db23456ed6
Sherwin Yu <sherwin.x.yu@gmail.com>
2013-10-08 17:22:02 -0400

    list view

--- src/com/sherwinyu/videotable/MainActivity.java
+++ src/com/sherwinyu/videotable/MainActivity.java
@@ -1,16 +1,43 @@
 package com.sherwinyu.videotable;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import android.app.Activity;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 public class MainActivity extends Activity {
 
+  ListView listView;
+  StableArrayAdapter adapter;
+  ArrayList<String> list ;
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
     setContentView(R.layout.activity_main);
+
     new RetrieveJsonTask().execute("asdf");
+    // setupAdapter();
+
+    String[] values = new String[] {"abc", "def", "ghi"};
+    list = new ArrayList<String>();
+    for (int i = 0; i < values.length; i++) {
+      list.add(values[i]);
+    }
+
+    listView = (ListView) findViewById(R.id.listView);
+    adapter = new StableArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+    listView.setAdapter(adapter);
+
+
   }
 
   @Override
@@ -19,5 +46,30 @@ public class MainActivity extends Activity {
     return true;
   }
 
+  private class StableArrayAdapter extends ArrayAdapter<String> {
+
+    HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+    public StableArrayAdapter(Context context, int textViewResourceId,
+        List<String> objects) {
+      super(context, textViewResourceId, objects);
+      for (int i = 0; i < objects.size(); ++i) {
+        mIdMap.put(objects.get(i), i);
+      }
+    }
+
+    @Override
+    public long getItemId(int position) {
+      String item = getItem(position);
+      return mIdMap.get(item);
+    }
+
+    @Override
+    public boolean hasStableIds() {
+      return true;
+    }
+
+  }
 
 }
+
